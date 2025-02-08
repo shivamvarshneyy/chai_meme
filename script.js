be005aa52fe8137de7a2880fcbb6b95d3edbec20
shivamvarshneyy <shivamvarshney1506@gmail.com>
2025-02-08 18:39:32 +0530

    meme

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const sound = new Audio("./chai_garam.mp3");
+    sound.play();
     const chai = document.querySelector(".chai");
     const hover = document.querySelector(".hover-tea");
 
-    console.log(chai);
-    console.log(hover);
-
-    const sound = new Audio("./chai_garam.mp3");
     const hover_sound = new Audio("./garam_hain_garam_hain.mp3");
 
-    chai.addEventListener("mouseenter",()=>{
-        sound.currentTime = 0;
+    chai.addEventListener("load",()=>{
+        sound.pause;
         sound.play();
         sound.loop = true;
     });
@@ -23,9 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     hover.addEventListener("mouseleave",()=>{
         hover_sound.pause();
-        sound.currentTime = 0;
-        sound.play();
-        sound.loop = true;
     });
 
 });
